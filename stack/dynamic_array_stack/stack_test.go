f015024b60f73cfd8e0d2076ff8b591b8f21a63f
jonny <jonnywamsley@github.com>
2021-04-12 23:46:50 -0400

    all stacks have a consistent method signature with better comments

--- stack/dynamic_array_stack/stack_test.go
+++ stack/dynamic_array_stack/stack_test.go
@@ -18,9 +18,9 @@ func TestPush(t *testing.T) {
 	i1 := s.Push(1)
 	i2 := s.Push(3)
 	i3 := s.Push(5)
-	assert.EqualValues(t, &Item{Data: 1}, i1)
-	assert.EqualValues(t, &Item{Data: 3}, i2)
-	assert.EqualValues(t, &Item{Data: 5}, i3)
+	assert.EqualValues(t, Item{Data: 1}, i1)
+	assert.EqualValues(t, Item{Data: 3}, i2)
+	assert.EqualValues(t, Item{Data: 5}, i3)
 
 	assert.EqualValues(t, 3, s.Len())
 }
@@ -30,7 +30,7 @@ func TestPopWithError(t *testing.T) {
 	item, err := s.Pop()
 	assert.NotNil(t, err)
 	assert.EqualValues(t, errors.New("error: stack is empty, can not pop from stack"), err)
-	assert.Equal(t, &Item{}, item)
+	assert.Equal(t, nil, item)
 
 	assert.EqualValues(t, 0, s.Len())
 	assert.True(t, s.Empty())
@@ -38,7 +38,7 @@ func TestPopWithError(t *testing.T) {
 	peeked, err := s.Peek()
 	assert.NotNil(t, err)
 	assert.EqualValues(t, errors.New("error: stack is empty"), err)
-	assert.Equal(t, &Item{}, peeked)
+	assert.Equal(t, nil, peeked)
 }
 
 func TestPopWithNoError(t *testing.T) {
@@ -48,13 +48,13 @@ func TestPopWithNoError(t *testing.T) {
 	s.Push(5)
 	item, err := s.Pop()
 	assert.Nil(t, err)
-	assert.EqualValues(t, 5, item.Data)
+	assert.EqualValues(t, 5, item)
 	assert.EqualValues(t, 2, s.Len())
 	assert.False(t, s.Empty())
 
 	peeked, err := s.Peek()
 	assert.Nil(t, err)
-	assert.EqualValues(t, 3, peeked.Data)
+	assert.EqualValues(t, 3, peeked)
 }
 
 func TestPeekWithError(t *testing.T) {
@@ -62,12 +62,12 @@ func TestPeekWithError(t *testing.T) {
 	peeked, err := s.Peek()
 	assert.NotNil(t, err)
 	assert.EqualValues(t, errors.New("error: stack is empty"), err)
-	assert.Equal(t, &Item{}, peeked)
+	assert.Equal(t, nil, peeked)
 }
 func TestPeekWithNoError(t *testing.T) {
 	s := New()
 	s.Push(1)
 	peeked, err := s.Peek()
 	assert.Nil(t, err)
-	assert.EqualValues(t, 1, peeked.Data)
+	assert.EqualValues(t, 1, peeked)
 }
--- stack/fixed_array_stack/stack_test.go
+++ stack/fixed_array_stack/stack_test.go
@@ -22,7 +22,8 @@ func TestNewNil(t *testing.T) {
 func TestPushNoError(t *testing.T) {
 	s := New(3)
 	for i := 0; i < 3; i += 1 {
-		err := s.Push(i)
+		item, err := s.Push(i)
+		assert.EqualValues(t, Item{Data: i}, item)
 		assert.Nil(t, err)
 	}
 	assert.EqualValues(t, 3, s.Len())
@@ -31,10 +32,12 @@ func TestPushNoError(t *testing.T) {
 func TestPushError(t *testing.T) {
 	s := New(3)
 	for i := 0; i < 3; i += 1 {
-		err := s.Push(i)
+		item, err := s.Push(i)
+		assert.EqualValues(t, Item{Data: i}, item)
 		assert.Nil(t, err)
 	}
-	err := s.Push(5)
+	item, err := s.Push(5)
+	assert.EqualValues(t, Item{}, item)
 	assert.NotNil(t, err)
 	assert.EqualValues(t, errors.New("error: stack is full, can not push onto stack"), err)
 }
@@ -44,17 +47,24 @@ func TestPopWithError(t *testing.T) {
 	item, err := s.Pop()
 	assert.NotNil(t, err)
 	assert.EqualValues(t, errors.New("error: stack is empty, can not pop from stack"), err)
-	assert.Equal(t, Item{}, item)
+	assert.Equal(t, nil, item)
 }
 
 func TestPopWithNoError(t *testing.T) {
 	s := New(3)
-	_ = s.Push(1)
-	_ = s.Push(3)
-	_ = s.Push(5)
-	item, err := s.Pop()
+	item, err := s.Push(1)
+	assert.EqualValues(t, Item{Data: 1}, item)
 	assert.Nil(t, err)
-	assert.EqualValues(t, 5, item.Data)
+	item, err = s.Push(3)
+	assert.EqualValues(t, Item{Data: 3}, item)
+	assert.Nil(t, err)
+	item, err = s.Push(5)
+	assert.EqualValues(t, Item{Data: 5}, item)
+	assert.Nil(t, err)
+
+	data, err := s.Pop()
+	assert.Nil(t, err)
+	assert.EqualValues(t, 5, data)
 	assert.EqualValues(t, 2, s.Len())
 	assert.False(t, s.Empty())
 }
@@ -64,12 +74,15 @@ func TestPeekWithError(t *testing.T) {
 	peeked, err := s.Peek()
 	assert.NotNil(t, err)
 	assert.EqualValues(t, errors.New("error: stack is empty"), err)
-	assert.Equal(t, Item{}, peeked)
+	assert.Equal(t, nil, peeked)
 }
 func TestPeekWithNoError(t *testing.T) {
 	s := New(3)
-	_ = s.Push(1)
+	item, err := s.Push(1)
+	assert.EqualValues(t, Item{Data: 1}, item)
+	assert.Nil(t, err)
+
 	peeked, err := s.Peek()
 	assert.Nil(t, err)
-	assert.EqualValues(t, 1, peeked.Data)
+	assert.EqualValues(t, 1, peeked)
 }
